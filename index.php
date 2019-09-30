32f0d57da5944dd72132ac7ca0296042f4403323
dblancoformacion <dblancoformacion@gmail.com>
2019-09-30 19:51:38 +0200

    Timestamp

--- index.php
+++ index.php
@@ -3,7 +3,9 @@ David (profe)
 <?php
 
 $f=fopen('visitas.txt','a');
-fwrite($f,$_SERVER['REMOTE_ADDR']."\r\n");
+fwrite($f,date('Y-m-d H:i:s '));
+fwrite($f,$_SERVER['REMOTE_ADDR']);
+fwrite($f,"\r\n");
 fclose($f)
 
 ?>
